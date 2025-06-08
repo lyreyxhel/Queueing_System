683595b35194aeb536ce8583d963a92b2edfbac2
Fritz <160713554+lyreyxhel@users.noreply.github.com>
2025-06-08 19:31:10 +0800

    Add files via upload

---
+++ QueueFunctions.cpp
@@ -0,0 +1,8 @@
+﻿#include "pch.h"         // ✅ Required if precompiled headers are enabled
+#include <ctime>
+#include <cstdlib>
+
+extern "C" __declspec(dllexport) int GenerateQueueNumber() {
+    srand((unsigned)time(0));
+    return 1000 + rand() % 9000;
+}
